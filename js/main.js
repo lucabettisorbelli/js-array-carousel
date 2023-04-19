5d0fa901325f95db4f83f8f993aadc3c0c1e64b8
luca betti sorbelli <luca betti sorbelli>
2023-04-19 16:01:01 +0200

    bonus 1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,81 @@
-
-// let containerElement = document.getElementById("container");
-// let sliderElement = document.getElementById("box");
-let slideElements = document.getElementsByClassName("slide");
+let ArrayImmagini = document.getElementsByClassName("slide");
 let btnBack = document.getElementById("btnBack");
 let btnNext = document.getElementById("btnNext");
-let currentSlide = 0;
+let immagineCorrente = 0;
+
+btnBack.addEventListener("click", function () {
+    // al click, se l'immagine visualizzata è maggiore di 0 (la prima immagine) allora scala di una, altrimenti (meno di 0) ritorna all'ultima (4)
+    if (immagineCorrente > 0) {
+        immagineCorrente--;
+    } else {
+        immagineCorrente = 4;
+    }
+
+    for (let i = 0; i < ArrayImmagini.length; i++) {
+        const slide = ArrayImmagini[i];
+        
+        if(i == immagineCorrente) {
+            slide.classList.remove("hidden");
+        } else {
+            slide.classList.add("hidden");
+        }
+    }
+})
+
+// prova in lezione con Luca
+
+// btnBack.addEventListener("click", function () {
+//     if (immagineCorrente>0) {
+//     for (let i = 0; i < ArrayImmagini.length; i++) {
+//         const slide = ArrayImmagini[i];
+        
+//         if(i == immagineCorrente-1) {
+//             slide.classList.remove("hidden");
+//         } else {
+//             slide.classList.add("hidden");
+//         }
+//     }
+//     } else {
+        
+//     }
+
+    // immagineCorrente--;
+// })
+
+btnNext.addEventListener("click", function () {
+    // se l'immagine corrente è minore della lunghezza dell'Array(-1), aumenta di uno, ritorna a 0; 
+    if (immagineCorrente < ArrayImmagini.length -1) {
+        immagineCorrente++;
+    } else {
+        immagineCorrente = 0;
+    }
+
+    for (let i = 0; i < ArrayImmagini.length; i++) {
+        const slide = ArrayImmagini[i];
+        
+        if(i == immagineCorrente) {
+            slide.classList.remove("hidden");
+        } else {
+            slide.classList.add("hidden");
+        }
+    }
+})
+
+//prova in lezione con Luca
+
+// btnNext.addEventListener("click", function () {
+//     if (immagineCorrente<4) {
+//     for (let i = 0; i < ArrayImmagini.length-1; i++) {
+//         const slide = ArrayImmagini[i];
+        
+//         if(i == immagineCorrente+1) {
+//             slide.classList.remove("hidden");
+//         } else {
+//             slide.classList.add("hidden");
+//         }
+//     }
+//     }
+
+//     immagineCorrente++;
+// })
+
