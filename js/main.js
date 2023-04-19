74c5abd2b105d4a85a6251c18fe56a8bbc7f486d
luca betti sorbelli <luca betti sorbelli>
2023-04-19 14:57:50 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+// let containerElement = document.getElementById("container");
+// let sliderElement = document.getElementById("box");
+let slideElements = document.getElementsByClassName("slide");
+let btnBack = document.getElementById("btnBack");
+let btnNext = document.getElementById("btnNext");
+let currentSlide = 0;
